9a32ee4ceffa2ee8b80d13b2788023a5bbfdc073
Romain <romain.pillot@epitech.eu>
2018-03-04 23:18:26 +0100

    fixed error handlers

--- main.cpp
+++ main.cpp
@@ -19,15 +19,20 @@ int main(int ac, char **args)
 	nts::ComponentFactory factory;
 	nts::NtsManager manager;
 
-	if (ac < 2)
-		throw nts::ParsingError("No file found as argument");
+    try {
+        if (ac < 2)
+            throw nts::ParsingError("No file found as argument");
 
-	nts::Parser parser(factory, manager, args[1]);
-	parser.run();
-	nts::ConsoleAnalyser console(manager, (size_t) ac, args);
-	console.parse_options();
-	manager.Simulate();
-	manager.Display();
-	console.init_analyser();
+        nts::Parser parser(factory, manager, args[1]);
+        parser.run();
+        nts::ConsoleAnalyser console(manager, (size_t) ac, args);
+        console.parse_options();
+        manager.Simulate();
+        manager.Display();
+        console.init_analyser();
+    } catch (const nts::NtsError& error) {
+        std::cerr << "An error occurred: " << error.what() << std::endl;
+        return (84);
+    }
 	return (0);
 }
